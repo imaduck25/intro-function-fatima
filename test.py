8b35713179f4d54e5bc381edf94c066e54047bb9
imaduck25 <fatimaa158@nycstudents.net>
2025-09-11 07:15:20 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,24 @@ import turtle
 from turtle import *
 t = Turtle()
 
-
 t.shape('turtle')
+
+t.forward(200)
+
+turtle.done
+
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+
+turtle.done
 
 def equal(x):
     t.forward(x)
@@ -12,5 +28,39 @@ def equal(x):
     t.left(120)
     t.forward(x)
 equal(200) 
+
+turtle.done
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right()
+
+turtle.done
+
+def rectangle(x):
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+rectangle(200)
+
+turtle.done
+
+def equal(x):
+    t.left(120)
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+equal(100) 
 
 turtle.done
