2b70dbb56b8b510634e5dd095a40f626aa7b9586
imaduck25 <fatimaa158@nycstudents.net>
2025-09-12 07:31:49 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,65 +2,13 @@ import turtle
 from turtle import *
 t = Turtle()
 
-t.shape('turtle')
-
-t.forward(200)
-
-turtle.done
-
-def square(x):
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-square(200)
-
-turtle.done
-
-def equal(x):
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-equal(200) 
-
-turtle.done
-
-def right():
-    t.forward(100)
-    t.left(90)
-    t.forward(100)
-    t.left(135)
-    t.forward(142)
-right()
-
-turtle.done
-
-def rectangle(x):
-    t.forward(100)
-    t.left(90)
-    t.forward(125)
-    t.left(90)
-    t.forward(100)
-    t.left(90)
-    t.forward(125)
-    t.left(90)
-rectangle(200)
-
-turtle.done
-
-def equal(x):
-    t.left(120)
-    t.forward(90)
-    t.left(120)
-    t.forward(90)
-    t.left(120)
-    t.forward(90)
-equal(100) 
+for i in range(60):
+   sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+right(100,90)
 
 turtle.done
