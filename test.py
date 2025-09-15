2ed1e41ba06b67aaff156fced09da3c2c00598a0
imaduck25 <fatimaa158@nycstudents.net>
2025-09-15 07:20:30 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import turtle
 from turtle import *
+
 t = Turtle()
 
-for i in range(60):
-   sidelength = 100
-rotate = 90
-def square(x,y):
-    for i in range(4):
-        t.forward(x)
-        t.left(y)
-right(100,90)
+def square(x):
+    for i in range(60):
+        for i in range(4):
+            t.forward(100)
+            t.left(90)
+        t.right(5)
+square(any)
 
 turtle.done
