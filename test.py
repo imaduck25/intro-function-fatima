785a533e413d3e9e5395f9418187b6d1f39d69a6
imaduck25 <fatimaa158@nycstudents.net>
2025-09-16 07:05:04 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,23 @@ from turtle import *
 
 t = Turtle()
 
-def square(x):
-    for i in range(60):
+""" def square(x):
+    for i in range(73):
         for i in range(4):
             t.forward(100)
             t.left(90)
         t.right(5)
-square(any)
+square(any) """
+
+def equal(x):
+    for i in range(65):
+        for i in range(3):
+            t.forward(100)
+            t.left(120)
+        t.right(5)
+equal(any)
+        
+""" def right(x):
+    for i in range """
 
 turtle.done
