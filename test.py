48352f25ff827bcb1e96767989c6e7d35046d93e
imaduck25 <fatimaa158@nycstudents.net>
2025-09-16 07:16:24 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import turtle
 from turtle import *
 
 t = Turtle()
+t.speed('fastest')
 
 """ def square(x):
     for i in range(73):
@@ -11,15 +12,22 @@ t = Turtle()
         t.right(5)
 square(any) """
 
-def equal(x):
+""" def equal(x):
     for i in range(65):
         for i in range(3):
             t.forward(100)
             t.left(120)
         t.right(5)
-equal(any)
+equal(any) """
         
-""" def right(x):
-    for i in range """
+def right(x):
+    for i in range(65):
+        t.forward(100)
+        t.left(90)
+        t.forward(100)
+        t.left(135)
+        t.forward(142)
+    t.right(15)
+right(any)
 
 turtle.done
