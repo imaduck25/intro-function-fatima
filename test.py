2d7f1b55058fca0a8d6ed116fdf772041e1ae973
imaduck25 <fatimaa158@nycstudents.net>
2025-09-17 07:16:21 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import turtle
 from turtle import *
 
 t = Turtle()
-t.speed('fastest')
+""" t.speed('fastest') """
 
 """ def square(x):
     for i in range(73):
@@ -20,14 +20,39 @@ square(any) """
         t.right(5)
 equal(any) """
         
-def right(x):
-    for i in range(65):
+""" def right(x):
+    for i in range(15):
+        for i in range(1):
+             t.forward(100)
+             t.left(90)
+             t.forward(100)
+             t.left(135)
+             t.forward(142)
+        t.right(15)
+right(100) """
+
+""" def right(x):
+    for i in range(80):
+        for i in range(1):
+             t.forward(100)
+             t.left(90)
+             t.forward(100)
+             t.left(135)
+             t.forward(142)
+        t.right(3.5)
+right(100) """
+
+""" def addSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        addSquares(length, 90)
+        length += 25
+addSquares(5) """
+
+def star(x):
+    for i in range(5):
+        t.left(144)
         t.forward(100)
-        t.left(90)
-        t.forward(100)
-        t.left(135)
-        t.forward(142)
-    t.right(15)
-right(any)
+star(100)
 
 turtle.done
